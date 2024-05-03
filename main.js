6a48e03f859a7ef3955dd18ef8d41543a5d17b80
SofiaAbou <sofia.abouabdillah1@gmail.com>
2024-05-03 20:44:55 +0200

    presentazione

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active');
 
 };
-
-
-
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
